376684377296d408479e54d095ca50603e8d8f05
WangKai <123940232@qq.com>
2015-01-18 22:48:17 +0800

    练习Canvas旋转相关

--- CustomViewT/src/main/java/com/wk/customview/MainActivity.java
+++ CustomViewT/src/main/java/com/wk/customview/MainActivity.java
@@ -1,28 +1,55 @@
 package com.wk.customview;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.wk.customview.view.CustomView;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-    private CustomView customView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private  ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        View startBtn = findViewById(R.id.btn_start);
-        customView = (CustomView) findViewById(R.id.cv_custiom);
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_swiprefresh);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipRefreshLayout);
+        swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GREEN);
+        swipeRefreshLayout.setDistanceToTriggerSync(60);
+        swipeRefreshLayout.setProgressBackgroundColor(R.color.aaa);
+        swipeRefreshLayout.setSize(0);
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1, getData());
+        listView.setAdapter(adapter);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View v) {
-                new Thread(customView).start();
+            public void onRefresh() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
+    }
+
+    public List<String> getData() {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            list.add(String.format("这是第%d条数据", i));
+        }
+        return list;
     }
 
 
