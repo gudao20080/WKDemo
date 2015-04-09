6332876c3882f2fdb6d063d4476fd264946f8ed4
WangKai <123940232@qq.com>
2015-04-09 18:56:34 +0800

    修改Fresco

--- frescodeom/src/main/java/com/wk/frescodeom/MainActivity.java
+++ frescodeom/src/main/java/com/wk/frescodeom/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wk.frescodeom;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +15,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         Fresco.initialize(this);
         setContentView(R.layout.activity_main);
+
+        Uri uri = Uri.parse("http://frescolib.org/static/fresco-logo.png");
+        SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
+        draweeView.setImageURI(uri);
     }
 
 
