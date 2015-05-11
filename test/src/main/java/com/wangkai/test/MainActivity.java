81135010cb8d6a16fe357472fc3f25173eece97c
WangKai <123940232@qq.com>
2015-05-11 21:16:11 +0800

    d

--- test/src/main/java/com/wangkai/test/MainActivity.java
+++ test/src/main/java/com/wangkai/test/MainActivity.java
@@ -1,6 +1,10 @@
 package com.wangkai.test;
 
 import android.app.Activity;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +18,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        ColorMatrix colorMatrix = new ColorMatrix();
+//        colorMatrix.setRotate(0, 1);
+//        colorMatrix.setSaturation(100);
+//
+//        Paint paint = new Paint();
+//        paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+//
+//        Matrix matrix = new Matrix();
+//        matrix.setRotate(30);
+//        matrix.setTranslate(10, 200);
+//        matrix.setScale(2,2);
+//        matrix.setSkew(10, 10);
 
 
     }
