f11e6be05b5c3312c63f8552631ab969d14ab17a
WangKai <wangkai@vvpinche.com>
2015-06-25 17:55:12 +0800

    自定义View

--- CustomViewT/src/main/java/com/wk/customview/MainActivity.java
+++ CustomViewT/src/main/java/com/wk/customview/MainActivity.java
@@ -1,121 +1,37 @@
 package com.wk.customview;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.os.Parcel;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
 
-import com.wk.customview.view.RoundImageDrawable;
-
-import java.util.ArrayList;
-import java.util.List;
+import butterknife.OnClick;
 
 
-public class MainActivity extends ActionBarActivity {
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private  ListView listView;
-    private Handler handler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-
-            switch (msg.what) {
-
-
-            }
-
-
-
-            return false;
-        }
-    });
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_swiprefresh);
-
-        setContentView(R.layout.a);
-        View view = findViewById(R.id.roundView);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.b);
-
-
-        RoundImageDrawable drawable = new RoundImageDrawable(bitmap);
-        view.setBackgroundDrawable(drawable);
-
-
-//        view.setBackgroundDrawable(new RoundImageDrawable());
-//        view.setBackgroundDrawable(new RoundImageDrawable(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher)));
+        setContentView(R.layout.activity_swiprefresh);
+        butterknife.ButterKnife.inject(this);
 
 
 
-//        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipRefreshLayout);
-//        swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GREEN);
-//        swipeRefreshLayout.setDistanceToTriggerSync(60);
-//        swipeRefreshLayout.setProgressBackgroundColor(R.color.aaa);
-//        swipeRefreshLayout.setSize(0);
-//        listView = (ListView) findViewById(R.id.listView);
-//
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-//                android.R.id.text1, getData());
-//        listView.setAdapter(adapter);
-//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                try {
-//                    Thread.sleep(2000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                swipeRefreshLayout.setRefreshing(false);
-//            }
-//        });
-       /* setContentView(R.layout.activity_swiprefresh);
-
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipRefreshLayout);
-        swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GREEN);
-        swipeRefreshLayout.setDistanceToTriggerSync(60);
-        swipeRefreshLayout.setProgressBackgroundColor(R.color.aaa);
-        swipeRefreshLayout.setSize(0);
-        listView = (ListView) findViewById(R.id.listView);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                android.R.id.text1, getData());
-        listView.setAdapter(adapter);
-//        swipeRefreshLayout.setRefreshing(true);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                swipeRefreshLayout.setRefreshing(true);
-                try {
-                    Thread.sleep(5000);
-                    swipeRefreshLayout.setRefreshing(false);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-        });
-*/
 
     }
 
-    public List<String> getData() {
-        List<String> list = new ArrayList<>();
+    @OnClick(R.id.btn_round_bitmap)
+    public void goRoundActivity() {
+        Intent intent = new Intent(this, RoundBitmapActivity.class);
+        startActivity(intent);
+//        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
 
-        for (int i = 0; i < 30; i++) {
-            list.add(String.format("这是第%d条数据", i));
-        }
-        return list;
+    }
+
+    @OnClick(R.id.btn_touch_view)
+    public void goTouchViewActivity() {
+        Intent intent = new Intent(this, TouchViewActivity.class);
+        startActivity(intent);
+
     }
 
 
