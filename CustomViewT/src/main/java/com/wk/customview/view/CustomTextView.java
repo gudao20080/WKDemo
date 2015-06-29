feb5a852e057dcdb0a5c8693a04454c8b547420d
WangKai <wangkai@vvpinche.com>
2015-06-29 16:33:22 +0800

    custom_text

--- CustomViewT/src/main/java/com/wk/customview/view/CustomTextView.java
+++ CustomViewT/src/main/java/com/wk/customview/view/CustomTextView.java
@@ -9,6 +9,7 @@ import android.graphics.Rect;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.wk.customview.R;
@@ -32,11 +33,6 @@ public class CustomTextView extends View {
 
     public CustomTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-    }
-
-    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 
@@ -47,7 +43,9 @@ public class CustomTextView extends View {
         if (TextUtils.isEmpty(mText)) {
             mText = "text is empty";
         }
-        mTextSize = typedArray.getDimension(R.styleable.Custom_text_View_textSize, 30);
+        mTextSize = typedArray.getDimensionPixelSize(R.styleable.Custom_text_View_textSize,
+            (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, 40, getResources().getDisplayMetrics())
+        );
         typedArray.recycle();
 
         mTextPaint.setTextSize(mTextSize);
@@ -55,14 +53,48 @@ public class CustomTextView extends View {
         mBounds = new Rect();
         mTextPaint.getTextBounds(mText, 0, mText.length(), mBounds);
         mPaint.setColor(Color.YELLOW);
+
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+//        super.onDraw(canvas);
 
 //        canvas.drawRect(mBounds, mPaint);
-        canvas.drawRect(0, 0, mBounds.width(), mBounds.height(), mPaint);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+        canvas.drawText(mText, getMeasuredWidth() / 2 - mBounds.width() / 2, getMeasuredHeight() / 2 + mBounds.height() / 2, mTextPaint);
 
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int w, h;
+
+        if (MeasureSpec.EXACTLY == widMode) {
+            w = widSize;
+        }else {
+            int desired = getPaddingLeft() + mBounds.width() + getPaddingRight();
+            w = desired;
+        }
+
+        if (MeasureSpec.EXACTLY == heightMode) {
+            h = heightSize;
+        } else {
+            int desired = getPaddingBottom() + mBounds.height() + getPaddingTop();
+            h = desired;
+        }
+
+//        super.onMeasure(MeasureSpec.makeMeasureSpec(w, widMode), MeasureSpec.makeMeasureSpec(h, heightMode));
+        setMeasuredDimension(w, h);
     }
 }
