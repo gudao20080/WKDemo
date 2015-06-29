167d3546d153b305823984e5b04d4067d9aef582
WangKai <wangkai@vvpinche.com>
2015-06-29 12:22:53 +0800

    自定义View

--- CustomViewT/src/main/java/com/wk/customview/MainActivity.java
+++ CustomViewT/src/main/java/com/wk/customview/MainActivity.java
@@ -3,17 +3,23 @@ package com.wk.customview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ListView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
+    @InjectView(R.id.btn_my_text_view)
+    Button btnMyTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swiprefresh);
-        butterknife.ButterKnife.inject(this);
+        ButterKnife.inject(this);
         ListView listView = new ListView(this);
 
 
@@ -34,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @OnClick(R.id.btn_my_text_view)
+    public void goMyTextViewActivity() {
+        Intent intent = new Intent(this, CustomTextActivity.class);
+        startActivity(intent);
 
+    }
 
 }
---
+++ CustomViewT/src/main/java/com/wk/customview/view/CustomTextView.java
@@ -0,0 +1,68 @@
+package com.wk.customview.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.wk.customview.R;
+
+/**
+ * User: WangKai(123940232@qq.com)
+ * 2015-06-29 09:08
+ */
+public class CustomTextView extends View {
+    private Paint mPaint;
+    private TextPaint mTextPaint;
+    private int mTextColor;
+    private String mText;
+    private float mTextSize;
+    private Rect mBounds;
+
+
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Custom_text_View);
+
+        mTextColor= typedArray.getColor(R.styleable.Custom_text_View_textColor, Color.RED);
+        mText = typedArray.getString(R.styleable.Custom_text_View_text);
+        if (TextUtils.isEmpty(mText)) {
+            mText = "text is empty";
+        }
+        mTextSize = typedArray.getDimension(R.styleable.Custom_text_View_textSize, 30);
+        typedArray.recycle();
+
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setColor(mTextColor);
+        mBounds = new Rect();
+        mTextPaint.getTextBounds(mText, 0, mText.length(), mBounds);
+        mPaint.setColor(Color.YELLOW);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(mBounds, mPaint);
+
+
+    }
+}
